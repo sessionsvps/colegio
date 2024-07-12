33975db534284d6bc4fb0d461bfde1c2772d0807
Daniel <danielzavaletamego@gmail.com>
2024-07-12 09:47:21 -0500

    Creando ruta para acceder a malla curricular

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -47,6 +47,9 @@
                 <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Filtrar
                 </button>
+                <a href="{{ route('cursos.malla') }}" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                    Malla Curricular
+                </a>
             </form>
         @endif
 
--- resources/views/cursos/malla.blade.php
+++ resources/views/cursos/malla.blade.php
@@ -1,44 +1,47 @@
 @extends('layouts.main')
 
 @section('contenido')
-
-<table class="min-w-full bg-white">
-    <thead>
-        <tr>
-            <th class="py-2 px-4 border-b-2 border-gray-200 bg-gray-100 text-left text-sm leading-4 text-gray-600 uppercase">
-                Curso
-            </th>
-            <th class="py-2 px-4 border-b-2 border-gray-200 bg-gray-100 text-left text-sm leading-4 text-gray-600 uppercase">
-                Competencias
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($cursos as $curso)
-            @php
-                $competencias = $curso->getCompetencias();
-                $firstCompetencia = $competencias->shift();
-            @endphp
-            @if($firstCompetencia != null && $competencias->count()>=0)
-                <tr>
-                    <td class="py-2 px-4 border-b border-gray-200" rowspan="{{ $competencias->count() + 1 }}">
-                        {{$curso->descripcion}}
-                    </td>
-                    <td class="py-2 px-4 border-b border-gray-200">
-                        {{$firstCompetencia->descripcion}}
-                    </td>
-                </tr>
-                @foreach($competencias as $competencia)
+    <a href="{{ route('cursos.index') }}"
+        class="bg-red-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 inline-block">
+        Volver
+    </a>
+    <table class="min-w-full bg-white">
+        <thead>
+            <tr>
+                <th class="py-2 px-4 border-b-2 border-gray-200 bg-gray-100 text-left text-sm leading-4 text-gray-600 uppercase">
+                    Curso
+                </th>
+                <th class="py-2 px-4 border-b-2 border-gray-200 bg-gray-100 text-left text-sm leading-4 text-gray-600 uppercase">
+                    Competencias
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($cursos as $curso)
+                @php
+                    $competencias = $curso->getCompetencias();
+                    $firstCompetencia = $competencias->shift();
+                @endphp
+                @if($firstCompetencia != null && $competencias->count()>=0)
                     <tr>
+                        <td class="py-2 px-4 border-b border-gray-200" rowspan="{{ $competencias->count() + 1 }}">
+                            {{$curso->descripcion}}
+                        </td>
                         <td class="py-2 px-4 border-b border-gray-200">
-                            {{$competencia->descripcion}}
+                            {{$firstCompetencia->descripcion}}
                         </td>
                     </tr>
-                @endforeach
-            @endif
-        @endforeach
-    </tbody>
-</table>
+                    @foreach($competencias as $competencia)
+                        <tr>
+                            <td class="py-2 px-4 border-b border-gray-200">
+                                {{$competencia->descripcion}}
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+            @endforeach
+        </tbody>
+    </table>
 @endsection
 
 @section('scripts')
