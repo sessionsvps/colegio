0d12d7fee79aca848686ea856d8ed4d710942ef2
Daniel <danielzavaletamego@gmail.com>
2024-07-11 21:02:41 -0500

    Autenticacion y personalizacion del login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,9 +21,12 @@
                 <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
                     <div class="flex-1">
                         <div class="text-center">
-                            <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Brand</h2>
+                            <div class="mx-auto space-y-2">
+                                <div><img src="{{ asset('img/sideral-logo.jpg') }}" alt="Logo de sideral" class="mx-auto w-40 h-auto"></div>
+                                <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Sideral Carrión</h2>
+                            </div>
     
-                            <p class="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
+                            <p class="mt-3 text-gray-500 dark:text-gray-300">Inicia sesión para acceder a tu cuenta</p>
                         </div>
     
                         <div class="mt-8">
@@ -35,11 +38,10 @@
                     style="background-image: url('{{ asset('img/p1.png') }}');">
                     <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
                         <div>
-                            <h2 class="text-4xl font-bold text-white">Brand</h2>
+                            <h2 class="text-4xl font-bold text-white">Universidad Sideral Carrión</h2>
+                            <h3 class="text-xl font-bold text-white">Innovación, Conocimiento y Futuro</h3>
     
-                            <p class="max-w-xl mt-3 text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                In autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam
-                                temporibus molestiae</p>
+                            <p class="max-w-xl mt-3 text-gray-100">Bienvenidos a la Universidad Sideral Carrión. Nos dedicamos a la excelencia académica, la investigación avanzada y la formación de líderes comprometidos con el progreso sostenible. Aquí, tu futuro comienza hoy, wawita.</p>
                         </div>
                     </div>
                 </div>
