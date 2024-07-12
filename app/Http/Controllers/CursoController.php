df55e14e7b396cbd365aaa04d4306005849f4a1d
Daniel <danielzavaletamego@gmail.com>
2024-07-12 00:28:26 -0500

    Iniciando logica de mostrado de curso por roles

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -4,26 +4,49 @@ namespace App\Http\Controllers;
 
 use App\Models\Curso;
 use App\Models\Nivel;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission; 
 
 class CursoController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index(?Request $request)
     {
-        $filtranivel = $request->input('nivel_educativo');
-        $niveles = Nivel::all();
+        $auth = Auth::user()->id;
+        $user = User::findOrFail($auth);
 
-        if ($filtranivel == null || $filtranivel == 0) {
-            $cursos = Curso::where('esActivo','=',1)->paginate(10);
-        } else {
-            $cursos = Curso::where('esActivo','=', 1)->whereHas('niveles', function($query) use ($filtranivel) {
-                $query->where('curso_por_niveles.id_nivel','=',$filtranivel);
-            })->paginate(10)->appends(['nivel_educativo' => $filtranivel]);
-        }
-        return view('cursos.index',compact('cursos', 'niveles', 'filtranivel'));
+        switch(true) {
+            case $user->hasRole('Admin'):
+                $filtranivel = $request->input('nivel_educativo');
+                $niveles = Nivel::all();
+        
+                if ($filtranivel == null || $filtranivel == 0) {
+                    $cursos = Curso::where('esActivo','=',1)->paginate(10);
+                } else {
+                    $cursos = Curso::where('esActivo','=', 1)->whereHas('niveles', function($query) use ($filtranivel) {
+                        $query->where('curso_por_niveles.id_nivel','=',$filtranivel);
+                    })->paginate(10)->appends(['nivel_educativo' => $filtranivel]);
+                }
+                return view('cursos.index',compact('cursos', 'niveles', 'filtranivel'));   
+            break;
+            case $user->hasRole('Estudiante'):
+
+                //Mostrar cursos del estudiante
+
+            break;
+            case $user->hasRole('Docente'):
+
+                //Mostrar cursos que dicta el docente
+
+            break;
+            default:
+            break;
+        }      
     }
 
     /**
