09663c75c9ea3c0807523bb0d16910c2bfd04cb8
Daniel <danielzavaletamego@gmail.com>
2024-07-31 18:15:57 -0500

    Relacionando estudiante_seccion con las exoneraciones en el modelo

--- app/Models/Estudiante_Seccion.php
+++ app/Models/Estudiante_Seccion.php
@@ -5,18 +5,16 @@ namespace App\Models;
 use Awobaz\Compoships\Compoships;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Thiagoprz\CompositeKey\HasCompositeKey;
 
 class Estudiante_Seccion extends Model
 {
+    use HasFactory, Compoships, HasCompositeKey;
 
     protected $table = 'estudiante_secciones';
     protected $primaryKey = ['codigo_estudiante', 'año_escolar', 'user_id'];
     public $incrementing = false;
     public $timestamps = false;
-    
-    use HasFactory;
-    use Compoships;
-
     protected $guarded = [];
 
     public function estudiante()
@@ -27,5 +25,10 @@ class Estudiante_Seccion extends Model
     public function seccion()
     {
         return $this->belongsTo(Seccion::class, ['id_seccion', 'id_nivel', 'id_grado'], ['id_seccion', 'id_nivel', 'id_grado']);
+    }
+
+    public function exoneraciones()
+    {
+        return $this->hasMany(Exoneracion::class, ['codigo_estudiante','user_id'], ['codigo_estudiante','user_id']);
     }
 }
