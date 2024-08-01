f7965be92143b67a38fcaddb6c01ac284478bec8
Dejer <zulatecnico2020@gmail.com>
2024-08-01 03:25:42 -0500

    mejorando diseño de lista de estudiantes de la vista Docentes

--- resources/views/estudiantes/lista.blade.php
+++ resources/views/estudiantes/lista.blade.php
@@ -9,7 +9,50 @@
         </div>
     </div>
 
-    <div class="mt-10 md:mt-20 grid lg:grid-cols-1 gap-5">
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-md text-center text-gray-500 dark:text-gray-400">
+            <thead class="text-md text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Código
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nombres
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Acciones
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($estudiantes as $estudiante)
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $estudiante->codigo_estudiante }}
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $estudiante->estudiante->primer_nombre . ' ' . $estudiante->estudiante->otros_nombres . ' ' .
+                        $estudiante->estudiante->apellido_paterno . ' ' . $estudiante->estudiante->apellido_materno }}
+                    </td>
+                    <td class="px-6 py-4 text-right">
+                        <div class="flex justify-center">
+                            <a href="{{ route('boleta_notas.edit', ['codigo_estudiante' => $estudiante->codigo_estudiante, 'codigo_curso' => $curso->codigo_curso,'año_escolar' => $estudiante->año_escolar]) }}"
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Registrar Notas</a>
+                        </div>
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="4" class="px-6 py-4 text-center">
+                        No hay registros
+                    </td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+
+    {{-- <div class="mt-10 md:mt-20 grid lg:grid-cols-1 gap-5">
         <div class="flex items-center bg-gray-50 shadow-md rounded-lg p-6 space-x-4 w-full">
             <div class="w-full">
                 <table class="min-w-full bg-white">
@@ -35,7 +78,7 @@
                             </td>
                             <td class="py-2 px-4 border-b border-gray-200">
                                 <a href="{{ route('boleta_notas.edit', ['codigo_estudiante' => $estudiante->codigo_estudiante, 'codigo_curso' => $curso->codigo_curso,'año_escolar' => $estudiante->año_escolar]) }}"
-                                   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline">
+                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline">
                                     Registrar Notas
                                 </a>
                             </td>
@@ -49,5 +92,5 @@
                 </table>
             </div>
         </div>
-    </div>
+    </div> --}}
 @endsection
