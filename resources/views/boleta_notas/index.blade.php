f4254122335b73854ff5626cf5e25e366931f9a7
Alexander <ajvr3023@gmail.com>
2024-08-04 23:56:14 -0500

    Vista boletas

--- resources/views/boleta_notas/index.blade.php
+++ resources/views/boleta_notas/index.blade.php
@@ -120,27 +120,6 @@
                 </tbody>
             </table>
         </div>
-    </form>
-
-    @if($estudiante)
-        <div class="mt-10 grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div>
-                <label for="reporte" class="block text-sm font-medium text-gray-700">Seleccione un Formato</label>
-                <select id="reporte" name="reporte"
-                    class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-                    <option value="" selected disabled></option>
-                    <option value="0">PDF</option>
-                    <option value="1">EXCEL</option>
-                </select>
-            </div>
-            <div>
-                <a id="reportButton"
-                    class="md:mt-6 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full md:w-auto">
-                    Generar Reporte
-                </a>
-            </div>
-        </div>
-        <div class="mt-10 md:mt-20 flex items-center bg-gray-50 shadow-md rounded-lg p-6 space-x-4">
         <div class="mt-10">
             {{ $estudiantes->links() }}
         </div>
@@ -202,24 +181,6 @@
 @endsection
 
 @section('scripts')
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            var reporteSelect = document.getElementById('reporte');
-            var generateReportButton = document.getElementById('reportButton');
-
-            // Recuperar el código del estudiante del servidor y almacenarlo en una variable
-            var codigoEstudiante = "{{ $estudiante ? $estudiante->codigo_estudiante : '' }}";
-
-            reporteSelect.addEventListener('change', function () {
-                var selectedValue = this.value;
-                if (selectedValue == '1') { // EXCEL
-                    generateReportButton.href = "{{ route('export') }}";
-                } else if (selectedValue == '0') { // PDF
-                    generateReportButton.href = "{{ route('exportPdfNotas', ['codigo_estudiante' => '__codigo_estudiante__']) }}".replace('__codigo_estudiante__', codigoEstudiante);
-                }
-            });
-        });
-    </script>
     @if (!Auth::user()->hasRole('Estudiante_Matriculado'))
         <script>
             document.addEventListener('DOMContentLoaded', function() {
