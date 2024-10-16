82d301f49b8b9ea2c3791387456a5e7b416e72f3
DiegoVJ22 <zulatecnico2020@gmail.com>
2024-10-16 18:17:03 -0500

    agregando Readme y corrigiendo form de ediciones

--- resources/views/estudiantes/edit.blade.php
+++ resources/views/estudiantes/edit.blade.php
@@ -43,7 +43,7 @@
                             <input
                                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="dni" name="dni" type="text" placeholder="DNI" value="{{ old('dni', $estudiante->dni) }}" maxlength="8"
-                                pattern="[0-9]{8}">
+                                pattern="[0-9]{8}" readonly>
                             @error('dni')
                             <p class="text-red-500 text-xs italic">{{ $message }}</p>
                             @enderror
